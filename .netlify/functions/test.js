4d2ea7681ad317b0d2adb4f8360dbed2a578d5ec
fimion <fimion@gmail.com>
2019-05-25 14:42:01 -0400

    initial commit

---
+++ .netlify/functions/test.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+exports.handler = function(event, context, callback) {
+  // your server-side functionality
+
+  fs.readdir('/', (err, files) => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(files)
+    })
+  });
+
+
+}
